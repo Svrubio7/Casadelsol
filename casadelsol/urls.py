6e10afe74086f9c81091a20617be7761589f7375
Svrubio7 <sverdugo.ieu2022@student.ie.edu>
2025-07-25 15:19:27 +0200

    fixing server error

--- casadelsol/urls.py
+++ casadelsol/urls.py
@@ -19,11 +19,14 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/properties/', include('properties.urls')),
+    # Serve media files
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
     # Serve frontend for all other routes
     re_path(r'^.*$', views.serve_frontend, name='frontend'),
 ]
